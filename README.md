c14c2f97931181c0cfeea3928fa4ee56b2cb3f4e
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-06-09 16:34:16 +0530

    done

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Write a React component that implements a simple to-do list, with the ability to add and remove items.
+## To-Do List App Using React
 
-![output](https://storage.googleapis.com/acciojob-open-file-collections/todo_AdobeExpress.gif)
+The To Do App that we are going to build, will allow a user to add a task to a list of to-do items.
+
+Once the task is added, the user will be able to delete or edit it as required. 
+
+When user click on edit button a input field must be shown with button 'save' besides it. After clicking on save button, task must be updated.
+
+**NOTE:**
+
+Use classes : `add_tasks_section`,  `tasks_section`,  `task`,  `edit`,  `delete`,  `save`
+
+![output](https://storage.googleapis.com/acciojob-open-file-collections/ezgif.com-gif-maker.gif)
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,146 @@
+import React, { useState, useReducer, useRef } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function Lister(props) {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <div className="task list col-6 bg-light p-3 mt-3 justify-content-center align-self-center">
+        {props.item.name}
+      </div>
+      <div className="col-3 bg-light p-3 mt-3">
+        <button
+          className="edit btn btn-success w-100"
+          onClick={() => props.edit(props.index)}
+        >
+          Edit
+        </button>
+      </div>
+      <div className="col-3 bg-light p-3 mt-3">
+        <button
+          className="delete btn btn-danger w-100"
+          onClick={() => props.dlt(props.index)}
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default App
+function Editor(props) {
+  const [val, setVal] = useState(props.item.name);
+
+  return (
+    <>
+      <div className="col-9 bg-light p-3 mt-3 justify-content-center align-self-center">
+        <textarea
+          rows="1"
+          className="editTask w-100"
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+          value={val}
+        />
+      </div>
+      <div className="col-3 bg-light p-3 mt-3">
+        <button
+          className="save saveTask btn btn-success w-100"
+          onClick={() => props.edit({ index: props.index, val })}
+        >
+          Save
+        </button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  let mainInput = useRef(null);
+
+  const [input, controlInput] = useState("");
+
+  const [tasks, addTask] = useReducer((tasks, task) => {
+    let temp = [...tasks];
+    let tt = {
+      name: task,
+      edit: false,
+    };
+    if (task !== "") {
+      mainInput.current.value = "";
+      temp.push(tt);
+    }
+    return temp;
+  }, []);
+
+  const [edit1, editTaskState] = useReducer((edit1, index) => {
+    let temp = [...tasks];
+    temp[index].edit = true;
+    return temp;
+  }, "");
+
+  const [edit2, editTaskInfo] = useReducer((edit2, { index, val }) => {
+    let temp = [...tasks];
+    if (val !== "") {
+      temp[index].name = val;
+      temp[index].edit = false;
+    }
+    return temp;
+  }, "");
+
+  const [edit3, deleteTaskInfo] = useReducer((edit3, index) => {
+    let temp = [...tasks];
+    tasks.splice(index, 1);
+    return temp;
+  }, "");
+
+  return (
+    <div id="main" className="container">
+      <div className="add_tasks_section jumbotron row mb-0">
+        <div className="col-12 mb-3 p-2 text-center">
+          <h3>To Do List</h3>
+        </div>
+        <div className="offset-1 col-8">
+          <textarea
+            ref={mainInput}
+            rows="1"
+            className="w-100 h-100"
+            name="task"
+            id="task"
+            onChange={(e) => {
+              e.preventDefault();
+              controlInput(e.target.value);
+            }}
+          />
+        </div>
+        <div className="col-2">
+          <button
+            className="w-100 btn btn-primary"
+            id="btn"
+            onClick={() => {
+              addTask(input);
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div className="tasks_section row p-2">
+        {tasks.map((item, index) =>
+          item.edit ? (
+            <Editor key={index} item={item} index={index} edit={editTaskInfo} />
+          ) : (
+            <Lister
+              key={index}
+              item={item}
+              index={index}
+              edit={editTaskState}
+              dlt={deleteTaskInfo}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
